40c2a251927a99cd40261839c61fc7f6615379a4
Vinay Kokate <vinaynkokate@gmail.com>
2023-07-28 10:33:50 +0530

    testimonail slider

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import "./testimonial.css";
 const Testimonial = () => {
   const testimonials = [
@@ -21,22 +23,45 @@ const Testimonial = () => {
       desc: "After using Crater for just a few weeks, I'm completely sold. The AI-powered features are incredibly accurate and make my life so much easier",
     },
   ];
+  const [Review, SetReview] = useState(0);
   return (
     <div className="testimonial-section">
       <h3>10,000+ Users have signed up!</h3>
       <div className="testimonials">
+        <button
+          onClick={() => {
+            if (Review <= 0) {
+              SetReview(testimonials.length - 1);
+            } else {
+              SetReview(Review - 1);
+            }
+          }}
+        >
+          <ArrowBackIosIcon />
+        </button>
         <div>
-          <img src={testimonials[2].img} alt="" />
+          <img src={testimonials[Review].img} alt="" />
         </div>
-        <div>
+        <div className="reviewinformation">
           <div>
-            <p>{testimonials[2].desc}</p>
+            <p>{testimonials[Review].desc}</p>
           </div>
           <div>
-            <b>{testimonials[2].name}</b>
-            <span>{testimonials[2].Pro}</span>
+            <b>{testimonials[Review].name}</b>
+            <span>{testimonials[Review].Pro}</span>
           </div>
         </div>
+        <button
+          onClick={() => {
+            if (Review + 1 >= testimonials.length) {
+              SetReview(0);
+            } else {
+              SetReview(Review + 1);
+            }
+          }}
+        >
+          <ArrowForwardIosIcon />
+        </button>
       </div>
     </div>
   );
